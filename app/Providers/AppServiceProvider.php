adec24ce88d12056654f6cdc27e866695272550b
pk <ecollatio@gmail.com>
2023-08-27 17:07:20 +0200

    add OneCallApi request to app container

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\OpenWeatherMap\OneCallApiV3\ApiKey;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +15,11 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->isLocal()) {
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
         }
+
+        $this->app->bind(\App\Services\OpenWeatherMap\OneCallApiV3\Request::class, function () {
+            $apiKey = new ApiKey(config('openweathermap.api.one_call_3.api_key'));
+            return new \App\Services\OpenWeatherMap\OneCallApiV3\Request($apiKey);
+        });
     }
 
     /**
